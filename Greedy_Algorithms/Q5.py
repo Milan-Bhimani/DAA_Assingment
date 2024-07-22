f8e52d72658cd6dd33c7b46e0450c05fa5a41f83
Milan Bhimani <milanhbhimani@gmail.com>
2024-07-22 18:46:20 +0530

    changing q 5

--- Greedy_Algorithms/Q5.py
+++ Greedy_Algorithms/Q5.py
@@ -1,9 +1,18 @@
-def average_waiting_time(bt):
-    n = len(bt)
-    bt.sort()
-    waiting_time = 0
-    total_waiting_time = 0
-    for i in range(n):
-        total_waiting_time += waiting_time
-        waiting_time += bt[i]
-    return total_waiting_time // n
+class Item:
+    def __init__(self, wt, val, ind):
+        self.wt = wt
+        self.val = val
+        self.ind = ind
+        self.vwt = val / wt  
+
+def fractional_knapsack(w, items):
+    items.sort(key=lambda x: x.vwt, reverse=True) 
+    total_value = 0.0
+    for item in items:
+        if item.wt <= w:
+            w -= item.wt
+            total_value += item.val
+        else:
+            total_value += item.val * w / item.wt
+            break
+    return total_value
